60aab7dd8923a2e0e6803c45e6724ca8856adaf8
17seannnn <17seannnn@gmail.com>
2022-09-07 20:07:15 +0300

    Lua_AddNote(), Lua_Check() funcs

--- Source/ScriptModule.cpp
+++ Source/ScriptModule.cpp
@@ -1,4 +1,10 @@
 /* ====== INCLUDES ====== */
+extern "C"
+{
+#include "lauxlib.h"
+#include "lualib.h"
+}
+
 #include "ScriptModule.h"
 
 /* ====== VARIABLES ====== */
@@ -9,10 +15,46 @@ b32 ScriptModule::StartUp()
 {
     AddNote(PR_NOTE, "Module started");
 
+    L = luaL_newstate();
+    luaL_openlibs(L);
+
+    s32 res = luaL_dostring(L, "a = 7 + 11 + math.sin(0.5)");
+
+    if (Lua_Check(res))
+    {
+        lua_getglobal(L, "a");
+        if (lua_isnumber(L, -1))
+        {
+            f32 a = (f32)lua_tonumber(L, -1);
+            Lua_AddNote(PR_NOTE, "7 + 11 = %f", a);
+        }
+    }
+
     return true;
 }
 
 void ScriptModule::ShutDown()
 {
+    lua_close(L);
+
     AddNote(PR_NOTE, "Module shut down");
+}
+
+void ScriptModule::Lua_AddNote(s32 priority, const char* fmt, ...) const
+{
+    va_list vl;
+    va_start(vl, fmt);
+    g_debugLogMgr.VAddNote(CHANNEL_SCRIPT, priority, "Lua", fmt, vl);
+    va_end(vl);
+}
+
+b32 ScriptModule::Lua_Check(s32 res)
+{
+    if (res != LUA_OK)
+    {
+        Lua_AddNote(PR_WARNING, "Lua_Check(): %s", lua_tostring(L, -1));
+        return false;
+    }
+
+    return true;
 }
--- Source/ScriptModule.h
+++ Source/ScriptModule.h
@@ -1,16 +1,25 @@
 #ifndef SCRIPTMODULE_H_
 #define SCRIPTMODULE_H_
 
+extern "C"
+{
+#include "lua.h"
+}
+
 #include "EngineModule.h"
 
 class ScriptModule : public EngineModule
 {
+    lua_State* L;
 public:
     ScriptModule() : EngineModule("ScriptModule", CHANNEL_SCRIPT) {}
     virtual ~ScriptModule() {}
 
     b32 StartUp();
     void ShutDown();
+private:
+    void Lua_AddNote(s32 priority, const char* fmt, ...) const;
+    b32 Lua_Check(s32 res);
 };
 
 extern ScriptModule g_scriptModule;
