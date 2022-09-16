4dd92d4ab6d0a372600a86464bd827ab2b3bd30f
17seannnn <17seannnn@gmail.com>
2022-09-16 23:15:01 +0300

    lua fix

--- Bin/Scripts/Mission1.lua
+++ Bin/Scripts/Mission1.lua
@@ -3,10 +3,12 @@ dofile "Scripts/GraphicsDefines.lua"
 -- Defines
 
 -- Variables
-local entities = {}
 local textures = {}
 local sounds = {}
 local music = {}
+
+local states = {}
+local entities = {}
 
 function onEnter()
   GT_LOG(PR_NOTE, "Mission1 entered")
@@ -22,12 +24,16 @@ function onEnter()
   sounds["test"] = defineSound("Sounds/TestSound.wav")
   music["test"] = defineMusic("Music/TestMusic.mp3")
 
+  -- AI state
+  states["NPC"] = defineState("stateNPC")
+
   -- Player
   textures["player"] = defineTexture("Textures/Actors/Player.png", TW_ACTOR, TH_ACTOR)
   entities["player"] = addActor(10, 60, TW_ACTOR, TH_ACTOR, textures["player"])
 
   -- NPC
   entities["NPC"] = addActor(TW_LOCATION, TH_LOCATION - TH_ACTOR/2, TW_ACTOR, TH_ACTOR, textures["player"])
+  setActorState(entities["NPC"], states["NPC"])
 
   -- Camera
   setLevelSize(TW_LOCATION * 2, TH_LOCATION)
@@ -55,5 +61,14 @@ function handleInput()
   end
   if isKeyDown(GTK_D) then
     sendActorCmd(entities["player"], GTC_MOVE_RIGHT)
+  end
+end
+
+function stateNPC(actor)
+  local n = math.random(1, 4)
+  if     n == 1 then sendActorCmd(actor, GTC_MOVE_UP)
+  elseif n == 2 then sendActorCmd(actor, GTC_MOVE_LEFT)
+  elseif n == 3 then sendActorCmd(actor, GTC_MOVE_DOWN)
+  elseif n == 4 then sendActorCmd(actor, GTC_MOVE_RIGHT)
   end
 end
