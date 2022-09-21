7e6e3aa74924b250060154baffa839c09b52b7cd
17seannnn <17seannnn@gmail.com>
2022-09-21 17:12:26 +0300

    Basic console behaviour

--- Source/Engine/Console.cpp
+++ Source/Engine/Console.cpp
@@ -1,10 +1,21 @@
 /* ====== INCLUDES ====== */
+#include <string.h>
+
+#include "GraphicsModule.h"
+#include "GTUnit.h"
+
 #include "Console.h"
 
 /* ====== DEFINES ====== */
-#define CONSOLE_WIDTH 80
-#define CONSOLE_HEIGHT 14
-#define CONSOLE_BUFSIZE (CONSOLE_WIDTH * CONSOLE_HEIGHT)
+#define CONSOLE_STRING_WIDTH 80
+#define CONSOLE_STRING_HEIGHT 15
+#define CONSOLE_BUFSIZE ((CONSOLE_STRING_WIDTH * CONSOLE_STRING_HEIGHT) + 1)
+
+#define CONSOLE_INPUT_INDEX (CONSOLE_STRING_WIDTH * (CONSOLE_STRING_HEIGHT - 1))
+
+#define CONSOLE_UNIT_HEIGHT 2.5f
+
+static const char s_consolePrompt[] = "> ";
 
 /* ====== VARIABLES ====== */
 Console g_console;
@@ -14,9 +25,10 @@ b32f Console::StartUp()
 {
     // Allocate and init buffer
     m_buffer = new u8[CONSOLE_BUFSIZE];
-    memset(m_buffer, ' ', CONSOLE_BUFSIZE);
+    Clear();
+    Reset();
 
-    // Don't show by default
+    // Defaults
     m_bShown = false;
 
     AddNote(PR_NOTE, "Module started");
@@ -27,4 +39,78 @@ b32f Console::StartUp()
 void Console::ShutDown()
 {
     delete[] m_buffer;
+}
+
+void Console::Render() const
+{
+    // Draw console's background
+    SDL_Rect dest = { 0, 0, g_graphicsModule.GetScreenWidth(), g_graphicsModule.GetScreenHeight() / 2 };
+    g_graphicsModule.SetColor(0x00, 0x00, 0x00, 0x10);
+    g_graphicsModule.FillRect(&dest);
+
+    // Draw text
+    g_graphicsModule.DrawText(&dest, GraphicsModule::s_pConsoleFont, (const char*)m_buffer, { 255, 255, 255, 255 });
+}
+
+void Console::Print(const char* text)
+{
+    for (i32f i = 0; i < strlen(text); ++i)
+    {
+        // TODO(sean)
+    }
+}
+
+void Console::Input(i32f ch)
+{
+    // Erase on backspace
+    if (ch == SDLK_BACKSPACE)
+    {
+        if (m_currentInput > CONSOLE_INPUT_INDEX + strlen(s_consolePrompt))
+        {
+            --m_currentInput;
+            m_buffer[m_currentInput] = ' ';
+        }
+        return;
+    }
+
+    // Clear input on escape
+    if (ch == SDLK_ESCAPE)
+    {
+        Reset();
+        return;
+    }
+
+    // Return if there no space for new character
+    if (m_currentInput >= CONSOLE_BUFSIZE - 1)
+        return;
+
+    // Place character
+    m_buffer[m_currentInput] = (u8)ch;
+    ++m_currentInput;
+}
+
+void Console::Interpret()
+{
+    // Additional work on buffer...
+    // g_scriptModule.ConsoleInterpret(&m_buffer[...])
+
+    // Reset console's input
+    Reset();
+}
+
+void Console::Clear()
+{
+    // Clear buffer
+    memset(m_buffer, ' ', CONSOLE_BUFSIZE-1);
+    m_buffer[CONSOLE_BUFSIZE - 1] = 0;
+
+    // Reset row
+    m_currentRow = 0;
+}
+
+void Console::Reset()
+{
+    m_currentInput = CONSOLE_INPUT_INDEX + (s32)strlen(s_consolePrompt);
+    memset(&m_buffer[CONSOLE_INPUT_INDEX], ' ', CONSOLE_STRING_WIDTH);
+    memcpy(&m_buffer[CONSOLE_INPUT_INDEX], s_consolePrompt, strlen(s_consolePrompt));
 }
--- Source/Engine/Console.h
+++ Source/Engine/Console.h
@@ -7,20 +7,26 @@ class Console : public EngineModule
 {
     u8* m_buffer;
     b32 m_bShown;
+
+    s32 m_currentRow;   // Row for Print()
+    s32 m_currentInput; // Position in input's row
 public:
     Console() : EngineModule("Console", CHANNEL_LOGMGR) {}
 
     b32f StartUp();
     void ShutDown();
 
-    void Render() const {}
+    void Render() const;
 
     void Toggle(b32 bToggle) { m_bShown = bToggle; }
     b32 IsShown() const { return m_bShown; }
 
-    void Print(const char* text) {}
-    void Input(i32f ch) { AddNote(PR_NOTE, "%c", ch); }
-    void Interpret() { AddNote(PR_NOTE, "Interpreted"); }
+    void Print(const char* text);
+    void Input(i32f ch);
+    void Interpret();
+
+    void Clear();
+    void Reset();
 };
 
 extern Console g_console;
